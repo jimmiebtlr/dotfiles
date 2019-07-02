00e016dc8dafb92256f1558ada6bdf003f46bbed
Jimmie Butler <jimmiebtlr@gmail.com>
2019-07-02 09:27:38 -0500

    .

--- .install.sh
+++ .install.sh
@@ -31,7 +31,11 @@ git branch --set-upstream-to=origin/master master
 home-manager switch
 
 # Nix python doesn't seem to be working.
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 python3-pip flatpak
+
+# Doubt current nix setup works with gpu + chrome os
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install --user flathub com.unity.UnityHub
 
 echo "DON'T FORGET TO ADD SSH KEYS TO RELEVANT LOCATIONS"
 echo "ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C jimmiebtlr@gmail.com"
